10a442e753567bf67a97b9fa534b97412215d2fe
Regina Khamidullina <reginylia@mail.ru>
2022-04-10 20:19:04 +0300

    add new sections

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const accordions = document.getElementsByClassName("types-title");
+
+for (let i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+  });
+}
